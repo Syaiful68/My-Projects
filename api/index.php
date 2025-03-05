c0601c568005e83dafc364644850fec5dd27f046
Syaiful68 <syaifulSheva@gmail.com>
2025-03-05 12:33:10 +0700

    perbaiki bugs index

--- api/index.php
+++ api/index.php
@@ -1,11 +1,2 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handler(
-    $request = illuminate\http\Request::capture()
-);
-
-$response->send();
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
